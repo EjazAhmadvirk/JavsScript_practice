dbe661a68f2ff97b70389cfc2a7be0527ad39794
Ejaz Ahmad <ejazahmadvirk091@gmail.com>
2025-07-14 13:00:07 +0500

    Add files via upload

---
+++ first.js
@@ -0,0 +1,32 @@
+// String
+let name = "Alice";
+console.log(name);
+
+// Number
+let age = 25;
+console.log(age);
+
+// Boolean
+let isStudent = true;
+console.log(isStudent);
+
+// Undefined
+let x;
+console.log(x);
+
+// Null
+let y = null;
+console.log(y);
+
+// Object
+let person = { name: "Bob", age: 30 };
+console.log(person);
+
+// Array
+let numbers = [1, 2, 3, 4, 5];
+console.log(numbers);
+
+
+console.log("This is Pakistan Babe");
+
+alert("Welcome on The Web Page");
